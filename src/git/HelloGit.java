f882fb6567b6ae25da10a13a0ee1798ac021a612
ccitlw <839542864@qq.com>
2018-12-11 14:42:31 +0800

    2222222222222222222222222

--- src/git/HelloGit.java
+++ src/git/HelloGit.java
@@ -16,5 +16,8 @@ package git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
