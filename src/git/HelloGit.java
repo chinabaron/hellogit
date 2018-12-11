7b6602ccf3354de337cf5eb1b3e37244e7a1a20a
ccitlw <839542864@qq.com>
2018-12-11 10:35:54 +0800

    666666666666

---
+++ src/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ *Copyrigth (C),2015-2018 创原天地 
+ *FileName:HelloGit
+ *Author:ccit
+ *Version V1.0
+ *Date:2018/12/11 10:32
+ *Desciption:1111111111111111
+ *History:
+ *<Author>           <Time>           <Version>      <Desciption>
+ *
+ *
+ */
+
+package git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
